01f7eb743bd01cc27c9eb263026c3c6e0a708ee6
teejay74 <teejay744@gmail.com>
2021-04-15 21:40:49 +0500

    task added eslint disable error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,7 @@
 import useJsonFetch from '../hooks/useJsonFetch';
 
 export default function Error() {
+    // eslint-disable-next-line no-unused-vars
     const [data, loading, error] = useJsonFetch('http://localhost:7070/error');
 
     return (
